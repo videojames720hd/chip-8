fdfc95b78ecc1bf71dc6896aaed15904dc9c24b0
videojames720hd <30006120+videojames720hd@users.noreply.github.com>
2026-08-27 17:46:32 -0400

    refactored for SDL3

--- Makefile
+++ Makefile
@@ -1,10 +1,8 @@
 EXEC = CHIP8
-
 CC = g++
-
 OPT = -O2
 
-CFLAGS = -std=c++17 -Wall ${OPT} -lSDL2
+CFLAGS = -std=c++17 -Wall ${OPT} -lSDL3
 
 ${EXEC}:
 	${CC} ${CFLAGS} *.cpp -o ${EXEC}
--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <cmath>
-#include <SDL2/SDL.h>
+#include <SDL3/SDL.h>
 
 #include "chip8.hpp"
 
@@ -14,17 +14,21 @@ int main(int argc, char *argv[]) {
     }
     chip8 CHIP8 = chip8(argv[1]);
 
-    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER);
-    SDL_Window *window = SDL_CreateWindow("CHIP8", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
+    if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO)) {
+        std::cerr << "Could not init SDL: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+    SDL_Window *window = SDL_CreateWindow("CHIP8", WIDTH, HEIGHT, SDL_WINDOW_HIGH_PIXEL_DENSITY);
     if (!window) {
         std::cerr << "Could not create window: " << SDL_GetError() << std::endl;
         return 1;
     }
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, NULL);
     if (!renderer) {
         std::cerr << "Could not create renderer: " << SDL_GetError() << std::endl;
         return 1;
     }
+    SDL_SetRenderLogicalPresentation(renderer, WIDTH, HEIGHT, SDL_LOGICAL_PRESENTATION_LETTERBOX);
 
     SDL_Event windowEvent;
     bool running = true;
@@ -33,54 +37,54 @@ int main(int argc, char *argv[]) {
 
         if (SDL_PollEvent(&windowEvent)) {
             switch (windowEvent.type) {
-                case SDL_QUIT:
+                case SDL_EVENT_QUIT:
                     running = false;
                     continue;
                     break;
-                case SDL_KEYDOWN:
-                    switch (windowEvent.key.keysym.sym) {
+                case SDL_EVENT_KEY_DOWN:
+                    switch (windowEvent.key.key) {
                         case SDLK_1: CHIP8.keypad[0x1] = true; break;
                         case SDLK_2: CHIP8.keypad[0x2] = true; break;
                         case SDLK_3: CHIP8.keypad[0x3] = true; break;
                         case SDLK_4: CHIP8.keypad[0xC] = true; break;
 
-                        case SDLK_q: CHIP8.keypad[0x4] = true; break;
-                        case SDLK_w: CHIP8.keypad[0x5] = true; break;
-                        case SDLK_e: CHIP8.keypad[0x6] = true; break;
-                        case SDLK_r: CHIP8.keypad[0xD] = true; break;
+                        case SDLK_Q: CHIP8.keypad[0x4] = true; break;
+                        case SDLK_W: CHIP8.keypad[0x5] = true; break;
+                        case SDLK_E: CHIP8.keypad[0x6] = true; break;
+                        case SDLK_R: CHIP8.keypad[0xD] = true; break;
 
-                        case SDLK_a: CHIP8.keypad[0x7] = true; break;
-                        case SDLK_s: CHIP8.keypad[0x8] = true; break;
-                        case SDLK_d: CHIP8.keypad[0x9] = true; break;
-                        case SDLK_f: CHIP8.keypad[0xE] = true; break;
+                        case SDLK_A: CHIP8.keypad[0x7] = true; break;
+                        case SDLK_S: CHIP8.keypad[0x8] = true; break;
+                        case SDLK_D: CHIP8.keypad[0x9] = true; break;
+                        case SDLK_F: CHIP8.keypad[0xE] = true; break;
 
-                        case SDLK_z: CHIP8.keypad[0xA] = true; break;
-                        case SDLK_x: CHIP8.keypad[0x0] = true; break;
-                        case SDLK_c: CHIP8.keypad[0xB] = true; break;
-                        case SDLK_v: CHIP8.keypad[0xF] = true; break;
+                        case SDLK_Z: CHIP8.keypad[0xA] = true; break;
+                        case SDLK_X: CHIP8.keypad[0x0] = true; break;
+                        case SDLK_C: CHIP8.keypad[0xB] = true; break;
+                        case SDLK_V: CHIP8.keypad[0xF] = true; break;
                     }
                     break;
-                case SDL_KEYUP:
-                    switch (windowEvent.key.keysym.sym) {
+                case SDL_EVENT_KEY_UP:
+                    switch (windowEvent.key.key) {
                         case SDLK_1: CHIP8.keypad[0x1] = false; break;
                         case SDLK_2: CHIP8.keypad[0x2] = false; break;
                         case SDLK_3: CHIP8.keypad[0x3] = false; break;
                         case SDLK_4: CHIP8.keypad[0xC] = false; break;
 
-                        case SDLK_q: CHIP8.keypad[0x4] = false; break;
-                        case SDLK_w: CHIP8.keypad[0x5] = false; break;
-                        case SDLK_e: CHIP8.keypad[0x6] = false; break;
-                        case SDLK_r: CHIP8.keypad[0xD] = false; break;
+                        case SDLK_Q: CHIP8.keypad[0x4] = false; break;
+                        case SDLK_W: CHIP8.keypad[0x5] = false; break;
+                        case SDLK_E: CHIP8.keypad[0x6] = false; break;
+                        case SDLK_R: CHIP8.keypad[0xD] = false; break;
 
-                        case SDLK_a: CHIP8.keypad[0x7] = false; break;
-                        case SDLK_s: CHIP8.keypad[0x8] = false; break;
-                        case SDLK_d: CHIP8.keypad[0x9] = false; break;
-                        case SDLK_f: CHIP8.keypad[0xE] = false; break;
+                        case SDLK_A: CHIP8.keypad[0x7] = false; break;
+                        case SDLK_S: CHIP8.keypad[0x8] = false; break;
+                        case SDLK_D: CHIP8.keypad[0x9] = false; break;
+                        case SDLK_F: CHIP8.keypad[0xE] = false; break;
 
-                        case SDLK_z: CHIP8.keypad[0xA] = false; break;
-                        case SDLK_x: CHIP8.keypad[0x0] = false; break;
-                        case SDLK_c: CHIP8.keypad[0xB] = false; break;
-                        case SDLK_v: CHIP8.keypad[0xF] = false; break;
+                        case SDLK_Z: CHIP8.keypad[0xA] = false; break;
+                        case SDLK_X: CHIP8.keypad[0x0] = false; break;
+                        case SDLK_C: CHIP8.keypad[0xB] = false; break;
+                        case SDLK_V: CHIP8.keypad[0xF] = false; break;
                     }
                     break;
             }
@@ -89,7 +93,7 @@ int main(int argc, char *argv[]) {
         CHIP8.run_cycle();
 
         SDL_RenderClear(renderer);
-        SDL_Rect square = {.x = 0, .y = 0, .w = 10, .h = 10};
+        SDL_FRect square = {.x = 0, .y = 0, .w = 10, .h = 10};
         for (uint8_t i = 0; i < 32; ++i) {
             for (uint8_t j = 0; j < 64; ++j) {
                 square.x = j * 10;
@@ -103,7 +107,7 @@ int main(int argc, char *argv[]) {
 
                 // Draw pixel outline
                 SDL_SetRenderDrawColor(renderer, 0, 0, 0, 1);
-                SDL_RenderDrawRect(renderer, &square);
+                SDL_RenderRect(renderer, &square);
             }
         }
         SDL_RenderPresent(renderer);
